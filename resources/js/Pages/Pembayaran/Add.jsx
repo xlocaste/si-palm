35ea45268fc543a5833d67aacaff508ac64d4df3
xlocaste <irfandi.di122@gmail.com>
2025-05-08 17:00:38 +0700

    style:consisten layout add pembayaran

--- resources/js/Pages/Pembayaran/Add.jsx
+++ resources/js/Pages/Pembayaran/Add.jsx
@@ -27,10 +27,10 @@ export default function Add({ auth }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Tambah Pembayaran</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Tambah Invoice</h2>}
         >
-            <div className="py-12">
-                <div className="max-w-4xl mx-auto sm:px-6 lg:px-8">
+            <div className="min-h-screen bg-gray-100 p-6">
+                <div className="max-w-6xl mx-auto bg-white shadow rounded-lg p-6">
                     <div className="bg-white shadow-sm sm:rounded-lg p-6">
                         <form onSubmit={handleSubmit} className="grid grid-cols-2 items-center gap-6">
                             <div>
