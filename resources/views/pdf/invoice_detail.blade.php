db74dafb09d41f116dac94cdbb5a7073e385ac08
xlocaste <irfandi.di122@gmail.com>
2025-05-18 21:56:17 +0700

    fix:layout print invoice

--- resources/views/pdf/invoice_detail.blade.php
+++ resources/views/pdf/invoice_detail.blade.php
@@ -61,7 +61,7 @@
                                 <tr>
                                     <td style="width: 30%">Harga (Rp/Kg)</td>
                                     <td style="width: 2%">:</td>
-                                    <td style="width: 68%">{{ $invoice->kontrak->harga ?? '-' }}</td>
+                                    <td style="width: 68%">Rp.{{ number_format($invoice->kontrak->harga, 0, ',', '.') }}</td>
                                 </tr>
                                 <tr>
                                     <td style="width: 30%">Nilai</td>
@@ -73,16 +73,60 @@
                                 </tr>
                             </table>
                         </td>
-                        <td class="tbody;" style="border: 1px solid; vertical-align: bottom;">Rp.{{$invoice->nilai}}</td>
+                        <td class="tbody;" style="border: 1px solid; vertical-align: bottom;">
+                            <div>
+                                Rp.{{ number_format($invoice->nilai, 0, ',', '.') }}
+                            </div>
+                            <div>
+                                 Rp.{{ number_format($invoice->ppn, 0, ',', '.') }}
+                            </div>
+                        </td>
                     </tr>
                     <tr>
                         <td style="border: 1px solid"></td>
                         <td style="text-align: right; border: 1px solid;">Jumlah</td>
-                        <td style="text-align: left; border: 1px solid;">Rp.{{$invoice->nilai}}</td>
+                        <td style="text-align: left; border: 1px solid;">Rp.{{ number_format($invoice->jumlah, 0, ',', '.') }}</td>
                     </tr>
                 </tbody>
             </table>
+            <p style="margin-top: 10px">Terbilang : <a style="font-style: italic; font-weight: bold;">{{$invoice->terbilang}}</a></p>
+            <div style="font-style: italic; margin-top: 20px;">
+                <p>
+                    Jumlah tersebut harap disetor pada Bank Mandiri Cabang Pontianak Tanjungpura
+                </p>
+                <p>
+                    A.n PT Perkebunan Nusantara IV
+                </p>
+                <p>
+                    No. Rekg. 1050088001874
+                </p>
+            </div>
+            <div class="ttd">
+                <div class="isi">
+                    <p>PT, PERKEBUNAN NUSANTARA IV</p>
+                    <p>Senior Executive Vice President</p>
+                    <p style="text-decoration: underline; font-weight: 500; padding-top: 80px;">Muhammad Zulham Rambe</p>
+                    <p>SEVP Bussines Support</p>
+                </div>
+            </div>
         </div>
+        <table class="footer">
+            <tr class="tr">
+                <td style="padding: 0">
+                    <p style="color: blue; padding-right: 0; margin: 0;">AKLAK<a style="color: black; padding: 0; margin: 0;"> - Amanah, Kompeten, Harmonis, Loyal, Adaptif, Kolaboratif</a> </p>
+                    <p>Head Office Gedung Agro Plaza Lt. 8</p>
+                    <p>Jl. H.R Rasuma Said Kaiv X2 No.1</p>
+                    <p>Telp     : +62 21 31119000</p>
+                    <p>Email    : ptpnusantara4@ptpn4.co.id</p>
+                </td>
+                <td style="padding: 0" style="text-align: right;">
+                    <p style="font-weight: 500; padding: 0; margin: 0; ">Regional V - Pontianak</p>
+                    <p>Jl. Sultan Abdurahman 11, Pontianak 78113</p>
+                    <p>Telp : +62 561 734110</p>
+                    <p>Email : sekper@ptpn13.co.id</p>
+                </td>
+            </tr>
+        </table>
     </div>
 </body>
 </html>
